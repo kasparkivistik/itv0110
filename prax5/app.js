118e345e01394161da49a5dd7059a6aec4a8eaa2
kasparkivistik <kkivis@ttu.ee>
2017-12-12 11:09:28 +0200

    save something so things dont get freaky

--- prax5/app.js
+++ prax5/app.js
@@ -1,9 +1,56 @@
+var playerPicks = [];
+
+var paper = 0;
+var rock = 1;
+var scissors = 2;
 
 
+function play(choice) {
 
-function changeScore(win, lose) {
-    var score = $('$score').text();
-    win = win + parseInt(score.substr(0, score.indexOf('-')));
-    lose = lose + parseInt(score.substr(0, score.indexOf('-') + 1));
-    $('score').text(win + "-" + lose);
+    if (playerPicks.length < 3) {
+        makeRandomChoice(choice);
+    } else {
+        //control if player chose more than 3 same in a row
+        if (playerPicks[playerPicks.length - 1] === playerPicks[playerPicks.length - 2]
+            && playerPicks[playerPicks.length - 2] === playerPicks[playerPicks.length - 3]) {
+
+            console.log('other person TURN');
+
+            if (playerPicks[playerPicks.length - 1] === 0) {
+                makeTurn(choice, 2);
+            } else {
+                makeTurn(choice, playerPicks[playerPicks.length - 1] - 1);
+            }
+        } else {
+            makeRandomChoice(choice);
+        }
+
+    }
+    playerPicks.push(choice);
 }
+
+function makeTurn(playerChoice, secondPlayerChoice) {
+    if (!(playerChoice === 0 && secondPlayerChoice === 2)
+        && ((playerChoice === 2 && secondPlayerChoice === 0)
+            || playerChoice < secondPlayerChoice)) {
+        //WON
+        console.log('Win ' + playerChoice + ' ' + secondPlayerChoice);
+    } else if (playerChoice === secondPlayerChoice) {
+        //DRAW
+        console.log('Draw ' + playerChoice + ' ' + secondPlayerChoice);
+    } else {
+        //LOSE
+        console.log('Lose ' + playerChoice + ' ' + secondPlayerChoice);
+    }
+}
+
+$('#playerpaper').click(function () {
+    play(paper);
+});
+$('#playerrock').click(function () {
+    play(rock);
+});
+$('#playerscissors').click(function () {
+    play(scissors);
+});
+
